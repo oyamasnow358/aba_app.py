520e450385aa6274123803a40d6b8f8022896d11
oyamasnow358 <oyamayuuji596308@gmail.com>
2025-02-09 20:27:56 +0900

    XS

--- aba_app.py
+++ aba_app.py
@@ -1,6 +1,11 @@
-import os
-import streamlit as st
+import matplotlib as mpl
+import matplotlib.font_manager as fm
 
-font_path = os.path.abspath("ipaexg.ttf")
-st.write(f"フォントパス: {font_path}")
-st.write(f"フォントが存在するか: {os.path.exists(font_path)}")
+font_path = "ipaexg.ttf"
+if os.path.exists(font_path):
+    font_prop = fm.FontProperties(fname=font_path)
+    mpl.rcParams["font.family"] = font_prop.get_name()
+
+# 現在のフォント設定を確認
+import streamlit as st
+st.write(f"現在のmatplotlibフォント設定: {mpl.rcParams['font.family']}")
